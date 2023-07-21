c59a3c012ce5aabab4c8a7b8607dc2adb1a7b0eb
widiantaraa <wynwidiantara88@gmail.com>
2023-07-21 11:33:34 +0800

    edit menu

--- codes/index.php
+++ codes/index.php
@@ -41,7 +41,7 @@ $struktur = query("SELECT * FROM struktur_organisasi");
     
       <div class="navbar-nav">
         <a href="#hom">Home</a>
-        <a href="berita.php">Berita</a>
+        <a href="berita.php">Skripsi</a>
         <a href="profile.php">Profile</a>
         <a href="struktur.php">Struktur Organisasi</a>
         <a href="visi.php">Visi Dan Misi</a>
@@ -69,7 +69,7 @@ $struktur = query("SELECT * FROM struktur_organisasi");
 <!-- section start berita -->
 
     <section id="berita" class="berita">
-        <h1>Halaman Berita Edit </h1>
+        <h1>Halaman Berita  </h1>
         <div class="row">
              <div class="berita_kiri">
              <?php foreach($berita as $row): ?>
